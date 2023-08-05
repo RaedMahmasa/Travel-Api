a91fd68aaafac29454fa758bcec3168348995400
RaedMahmasa <raed.raed.mahmasa@gmail.com>
2023-08-05 19:32:06 +0300

    project Travel-Api with Php Laravel

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TourController;
+use App\Http\Controllers\Api\V1\TravelController;
+use App\Http\Controllers\Api\V1\Admin;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('travels',[TravelController::class,'index']);
+Route::get('travels/{travel:slug}/tours',[TourController::class,'index']);
+
+Route::prefix('admin')->middleware(['auth:sanctum','role:admin'])->group(function() {
+    Route::post('travels', [Admin\TravelController::class , 'store']);
+    Route::post('travels/{travel}/tours', [Admin\TourController::class , 'store']);
+
+
+});
+
+Route::post('login',LoginController::class);
+
+
+
